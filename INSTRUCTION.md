532c979d90b0a76d580a6f1a55f450449b854080
carl-erik gauthier <carlerik.gauthier@gmail.com>
2026-05-18 23:19:14 +0200

    feat:topic finder manual implementation done

---
+++ INSTRUCTION.md
@@ -0,0 +1,13 @@
+# Goal
+- Build an agentic workflow that collects papers from different ArXiv categories to generate a one-pager summary on a field of research specified by the user. If not specified, all fields should be considered
+- The one-paper contains at most 5 topics
+- Implementation must use openai agents SDK
+
+# Rules
+
+# Plan
+## Phase 1: 
+## Phase 2: 
+## Phase 3: 
+## Phase 4: 
+## Phase 5: 
--- src-_new/topic_finder.py
+++ src-_new/topic_finder.py
@@ -1,17 +1,17 @@
 import openai
 import os
 import tiktoken
+import pandas as pd
+from copy import deepcopy
 from sentence_transformers import SentenceTransformer
 from bertopic import BERTopic
 from bertopic.representation import OpenAI as OpenAIRepresentation
 from bertopic.representation import MaximalMarginalRelevance
 from dotenv import load_dotenv
+from typing import List
+from data_object import Paper
 
 load_dotenv(override=True)
-
-
-# Load a custom embedding model
-# embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 summarization_prompt = """
 I have a topic that is described by the following keywords: [KEYWORDS]
@@ -32,37 +32,87 @@ Based on the information above, extract a short topic label in the following for
 topic: <topic label>
 """
 
-# api_key=os.getenv('OPENAI_API_KEY')
-# client = openai.OpenAI(api_key=api_key)
-import tiktoken
+def compute_topics(papers: List[Paper], n_topics: int=1, n_papers_per_topic: int=3):
+    docs = [f"{p.title} -- {p.summary}" for p in papers]
+    api_key = os.getenv('OPENAI_API_KEY')
+    client = openai.OpenAI(api_key=api_key)
+    tokenizer= tiktoken.encoding_for_model("gpt-4o-mini")
 
+    topic_summary_representation_model = [
+        MaximalMarginalRelevance(diversity=0.3),
+        OpenAIRepresentation(
+            client,
+            model="gpt-4o-mini",
+            prompt=summarization_prompt,
+            chat=True,
+            nr_docs=10,
+            doc_length=200,
+            diversity=0.3,
+            tokenizer=tokenizer
+            )
+            ]
+    
+    topic_title_representation_model = [
+        MaximalMarginalRelevance(diversity=0.3),
+        OpenAIRepresentation(
+            client,
+            model="gpt-4o-mini",
+            prompt=topic_title_prompt,
+            chat=True,
+            nr_docs=10,
+            doc_length=200,
+            diversity=0.3,
+            tokenizer=tokenizer
+            )
+            ]
+    
+    representation_models = {
+        "Main": topic_summary_representation_model,
+        "topic_title_": topic_title_representation_model
+    }
+    embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Tokenizer
-# tokenizer= tiktoken.encoding_for_model("gpt-4o-mini")
-#representation_model = [MaximalMarginalRelevance(diversity=0.3),
-#                        OpenAIRepresentation(
-                            # client,
-                            # model="gpt-4o-mini",
-                            # prompt=summarization_prompt,
-                            # chat=True,
-                            # nr_docs=5,
-#                             doc_length=200,
-#                             tokenizer=tokenizer
-#                             )
-#                             ]
-# topic_model = BERTopic(
-#     embedding_model=embedding_model,
-#     representation_model=representation_model
-#     )
-# topics, probs = topic_model.fit_transform(docs)
-# topic_model.fit(docs)
-# topic_info.loc[1, 'Representation'][0]
+    # uses default parameter of Bertopic for dimension reduction and clustering
+    topic_model = BERTopic(
+        embedding_model=embedding_model,
+        representation_model=representation_models
+        )
+    
+    label, prob = topic_model.fit_transform(docs)
+    # get topic info and keep top n_topics
+    topic_info_base = topic_model.get_topic_info()
 
-# get_representative_docs() returns the most relevant documents per topic.
-# Generate topic labels	.generate_topic_labels()
-# .representative_docs_	The representative documents for each topic if HDBSCAN is used.
+    topic_info_base = deepcopy(topic_info_base[topic_info_base.Topic!=-1]).sort_values(by="Count", ascending=False)
+    topic_info = topic_info_base.head(n_topics)
+    topic_info.reset_index(drop=True, inplace=True)
+    topic_info.rename(columns={"Topic": "topic"}, inplace=True)
+    # find the most representative papers defined as the top n paper per topic
+    df = pd.DataFrame(data={
+        # "arxiv_id": [p.arxiv_id for p in papers],
+        "paper": papers,
+        "topic": label,
+        "probabilities": prob
+    })
 
-# https://maartengr.github.io/BERTopic/getting_started/representation/representation.html
-# https://maartengr.github.io/BERTopic/getting_started/representation/llm.html#prompt-engineering
-# https://github.com/MaartenGr/BERTopic/blob/master/bertopic/_bertopic.py
-# topic_model.get_topic_info()
+    paper_topic_df = df.merge(topic_info[["topic"]], how="inner", on="topic")
+    paper_topic_df['rk'] = paper_topic_df.sort_values(['probabilities'], ascending=[False]) \
+             .groupby(['topic']) \
+             .cumcount() + 1
+    paper_topic_df = deepcopy(paper_topic_df[paper_topic_df.rk<=n_papers_per_topic]).reset_index(drop=True)
+    representative_papers = paper_topic_df[["topic", "paper"]].groupby(by="topic").apply(list)
+
+    # combine all infos to return a list of dictionaries. The list length is equal to n_topics and 
+    # dictionaries are like
+    # {
+    #     "topic_title": <title of the topic>,
+    #     "nb_papers": <size of the topic>,
+    #     "topic_description": <a brief description about the content of the topic>,
+    #     "representative_papers": <a list of length n_papers_per_topic of Paper object>
+    # }
+    final_df = topic_info.merge(representative_papers, on="topic", how="inner")
+    final_df["topic_description"] = final_df["Representation"].apply(lambda arr: arr[0])
+    final_df["topic_title"] = final_df["topic_title_"].apply(lambda arr: arr[0])
+    final_df = deepcopy(final_df[["topic_title", "Count", "topic_description", "paper"]]).rename(
+        columns={"Count": "nb_papers", "paper": "representative_papers"})
+    
+    return final_df.to_dict("records")
